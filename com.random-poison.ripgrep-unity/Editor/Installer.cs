b98344db31672b52d3f21357fc92a3cdbee344d4
David LeGare <dlegare.1001@gmail.com>
2020-11-25 17:05:23 -0600

    Setup initial test for installion on Windows
    I've split the installation logic into a separate type `InstallOperation` that encapsulates the lifecycle of the installation. This will allow us to drive the installation programatticaly from tests (and other packages, if necessary).

--- com.random-poison.ripgrep-unity/Editor/Installer.cs
+++ com.random-poison.ripgrep-unity/Editor/Installer.cs
@@ -1,90 +1,42 @@
 using System.IO;
-using ICSharpCode.SharpZipLib.Core;
-using ICSharpCode.SharpZipLib.Zip;
 using UnityEditor;
-using UnityEngine.Networking;
-using Debug = UnityEngine.Debug;
 
 namespace Ripgrep.Editor
 {
     public class Installer
     {
-        internal const string DownloadUrl = "https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep-12.1.1-x86_64-pc-windows-msvc.zip";
-        internal const string InstallRoot = "Library/com.random-poison.ripgrep-unity";
-        internal const string BinPath = "Library/com.random-poison.ripgrep-unity/ripgrep-12.1.1-x86_64-pc-windows-msvc/rg.exe";
+        public const string InstallRoot = "Library/com.random-poison.ripgrep-unity";
+        public const string RipgrepVersion = "12.1.1";
+
+        public static readonly string WindowsDownloadUrl = $"https://github.com/BurntSushi/ripgrep/releases/download/{RipgrepVersion}/ripgrep-{RipgrepVersion}-x86_64-pc-windows-msvc.zip";
+        public static readonly string WindowsBinPath = $"Library/com.random-poison.ripgrep-unity/ripgrep-{RipgrepVersion}-x86_64-pc-windows-msvc/rg.exe";
 
         /// <summary>
         /// Checks if the Ripgrep binary has been installed.
         /// </summary>
-        public static bool IsInstalled => File.Exists(BinPath);
+        public static bool IsInstalled => File.Exists(WindowsBinPath);
 
         // TODO: Add a more script-friendly way to perform the install, that way other
         // scripts can trigger the install if they need ripgrep.
         [MenuItem("Tools/Ripgrep/Install Ripgrep")]
-        public static void TryToDoAnInstall()
+        public static void InstallMenuItem()
+        {
+            // TODO: Probably log any errors since there's no user code to handle them.
+            Install();
+        }
+
+        public static InstallOperation Install()
         {
             // TODO: Check to see if it's already installed and skip the installation
             // process if it is.
             //
             // TODO: Provide an option to force-install, which would mean deleting any
             // existing installation before doing the install.
-
+            //
             // TODO: Determine the correct download URL for the current platform.
-            var request = UnityWebRequest.Get(DownloadUrl);
-            request.SendWebRequest().completed += _ =>
-            {
-                if (request.isNetworkError || request.isHttpError)
-                {
-                    Debug.LogError($"Failed to download archive: {request.error}");
-                    return;
-                }
-
-                var downloadPath = Path.Combine(InstallRoot, "ripgrep.zip");
-
-                // Create the directory for the file if it doesn't already exist.
-                Directory.CreateDirectory(Path.GetDirectoryName(downloadPath));
-
-                // TODO: Use a more efficient method for transferring the download bytes.
-                // Ideally this would mean streaming the downloaded bytes directly into
-                // the unzipper, but if we have to unzip from a file then at least
-                // streaming the bytes into a file without buffering them in-memory would
-                // be an improvement.
-                File.WriteAllBytes(downloadPath, request.downloadHandler.data);
-
-                using (var fileInput = File.OpenRead(downloadPath))
-                using (var zipFile = new ZipFile(fileInput))
-                {
-                    foreach (ZipEntry zipEntry in zipFile)
-                    {
-                        // Ignore directories, since we handle recreating the directory
-                        // hierarchy based on the path described in the entry name.
-                        if (!zipEntry.IsFile)
-                        {
-                            continue;
-                        }
-
-                        var entryFileName = zipEntry.Name;
-
-                        // Determine the output path for the file.
-                        var fullZipToPath = Path.Combine(InstallRoot, entryFileName);
-
-                        // Create the directory for the file if it doesn't already exist.
-                        Directory.CreateDirectory(Path.GetDirectoryName(fullZipToPath));
-
-                        // 4K is optimum (according to the SharpZipLib examples).
-                        var buffer = new byte[4096];
-
-                        // Unzip file in buffered chunks. This is just as fast as unpacking
-                        // to a buffer the full size of the file, but does not waste memory.
-                        // The "using" will close the stream even if an exception occurs.
-                        using (var zipStream = zipFile.GetInputStream(zipEntry))
-                        using (Stream fsOutput = File.Create(fullZipToPath))
-                        {
-                            StreamUtils.Copy(zipStream, fsOutput, buffer);
-                        }
-                    }
-                }
-            };
+            var installOp = new InstallOperation(WindowsDownloadUrl, InstallRoot);
+            installOp.Start();
+            return installOp;
         }
     }
 }
