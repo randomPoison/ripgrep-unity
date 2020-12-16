6165d48f3f69cd38d45a8690f80a9474edeba683
David LeGare <dlegare.1001@gmail.com>
2020-12-16 16:24:33 -0600

    Setup custom QuickSearch provider

--- com.random-poison.find-references/Editor/FindReferences.cs
+++ com.random-poison.find-references/Editor/FindReferences.cs
@@ -1,14 +1,16 @@
 using System.Collections.Generic;
 using System.IO;
 using Ripgrep.Editor;
+using Unity.QuickSearch;
 using UnityEditor;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
 
 namespace FindReferences.Editor
 {
     public class FindReferences
     {
-        [MenuItem("Assets/Find All References")]
+        [MenuItem("Assets/Find All References (debug)")]
         public static void ForCurrentSelection()
         {
             if (Selection.assetGUIDs.Length == 0)
@@ -29,6 +31,26 @@ namespace FindReferences.Editor
                     Debug.Log($"Found {references.Count} reference(s) to {name} (took {elapsedTime:0.##} secs):\n" + string.Join("\n", references));
                 }
             }
+        }
+
+        [Shortcut("Quick Search/Find References")]
+        [MenuItem("Assets/Find References")]
+        public static void PopQuickSearch()
+        {
+            QuickSearch.OpenWithContextualProvider("references");
+        }
+
+        [SearchItemProvider]
+        internal static SearchProvider CreateProvider()
+        {
+            return new SearchProvider("references", "Find References")
+            {
+                filterId = "ref:",
+                fetchItems = (context, items, provider) =>
+                {
+                    return null;
+                },
+            };
         }
 
         public static List<string> ForGuid(string guid)
