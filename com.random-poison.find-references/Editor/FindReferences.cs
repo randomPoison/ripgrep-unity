a871f81d624792bf05f5e8de852a1ce5653b47f7
David LeGare <dlegare.1001@gmail.com>
2021-01-22 11:02:25 -0600

    Integrate with Quick Search package (#3)

--- com.random-poison.find-references/Editor/FindReferences.cs
+++ com.random-poison.find-references/Editor/FindReferences.cs
@@ -1,14 +1,16 @@
 using System.Collections.Generic;
 using System.IO;
 using Ripgrep.Editor;
+using Unity.QuickSearch;
 using UnityEditor;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
 
 namespace FindReferences.Editor
 {
     public class FindReferences
     {
-        [MenuItem("Assets/Find All References")]
+        [MenuItem("Assets/Find All References (debug)")]
         public static void ForCurrentSelection()
         {
             if (Selection.assetGUIDs.Length == 0)
@@ -21,21 +23,76 @@ namespace FindReferences.Editor
                 var name = Path.GetFileName(AssetDatabase.GUIDToAssetPath(guid));
 
                 var startTime = Time.realtimeSinceStartup;
-                var references = ForGuid(guid);
-
-                var elapsedTime = Time.realtimeSinceStartup - startTime;
-                if (references != null)
+                ForGuid(guid).Completed += references =>
                 {
+                    var elapsedTime = Time.realtimeSinceStartup - startTime;
                     Debug.Log($"Found {references.Count} reference(s) to {name} (took {elapsedTime:0.##} secs):\n" + string.Join("\n", references));
-                }
+                };
             }
         }
 
-        public static List<string> ForGuid(string guid)
+        [Shortcut("Quick Search/Find References")]
+        [MenuItem("Assets/Find References")]
+        public static void PopQuickSearch()
+        {
+            if (Selection.assetGUIDs.Length > 0)
+            {
+                var search = QuickSearch.OpenWithContextualProvider("references");
+                search.SetSearchText("ref:" + AssetDatabase.GUIDToAssetPath(Selection.assetGUIDs[0]));
+            }
+        }
+
+        [SearchItemProvider]
+        internal static SearchProvider CreateProvider()
+        {
+            return new SearchProvider("references", "Find References")
+            {
+                filterId = "ref:",
+                isExplicitProvider = true,
+                fetchItems = (context, items, provider) => FetchItems(context, provider),
+                toObject = (item, type) => AssetDatabase.LoadAssetAtPath(item.id, type),
+                openContextual = (selection, rect) => OpenContextualMenu(selection, rect),
+
+                showDetails = true,
+                showDetailsOptions = ShowDetailsOptions.Default | ShowDetailsOptions.Inspector,
+            };
+        }
+
+        private static IEnumerable<SearchItem> FetchItems(SearchContext context, SearchProvider provider)
+        {
+            var guid = AssetDatabase.AssetPathToGUID(context.searchQuery);
+            if (guid == null)
+            {
+                yield break;
+            }
+
+            var search = ForGuid(guid);
+            while (!search.IsDone)
+            {
+                yield return null;
+            }
+
+            foreach (var assetPath in search.Result)
+            {
+                yield return provider.CreateItem(context, assetPath, assetPath, null, null, null);
+            }
+        }
+
+        public static Search ForGuid(string guid)
         {
             var search = new Search();
             search.Args = $@"--files-with-matches --no-text --glob !**/*.meta ""{guid}"" Assets/";
-            return search.Run();
+            search.Run();
+            return search;
+        }
+
+        private static bool OpenContextualMenu(SearchSelection selection, Rect contextRect)
+        {
+            var old = Selection.instanceIDs;
+            SearchUtils.SelectMultipleItems(selection);
+            EditorUtility.DisplayPopupMenu(contextRect, "Assets/", null);
+            EditorApplication.delayCall += () => EditorApplication.delayCall += () => Selection.instanceIDs = old;
+            return true;
         }
     }
 }
